5e636503822a84cb8f326750d67a125500ffc71a
DevTiqo <lilfreshy33@yahoo.com>
2022-03-10 13:16:22 +0100

    ves

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class _MyAppState extends State<MyApp> {
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'NewsAppYsasss',
+      title: 'NewsAppYsass',
       theme: light,
       home: AuthenticationWrapper(),
       routes: {
