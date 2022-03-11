a497d4f9511802a1e54bd5a803ff556b302aa7d4
DevTiqo <lilfreshy33@yahoo.com>
2022-03-11 16:47:10 +0100

    htg

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class _MyAppState extends State<MyApp> {
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'NewsAppYsaa',
+      title: 'NewsAppYsaavd',
       theme: light,
       home: AuthenticationWrapper(),
       routes: {
